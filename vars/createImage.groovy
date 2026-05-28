d00503292c8acbe370d86326c075336b77fef62f
unknown <omotolaadebulu@gmail.com>
2026-05-28 22:05:27 +0100

    updated logics

--- vars/createImage.groovy
+++ vars/createImage.groovy
@@ -2,5 +2,5 @@
 
 def call() {
     echo "creating the docker image..."
-    sh "docker build -t sunesis003/app-jenkins:5.0 ."
+    sh "docker build -t sunesis003/app-jenkins:jsl-6.0 ."
 }
--- vars/publishImage.groovy
+++ vars/publishImage.groovy
@@ -5,6 +5,6 @@ def call() {
     echo "publishing to docker hub repo..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push sunesis003/app-jenkins:5.0"
+        sh "docker push sunesis003/app-jenkins:jsl-6.0"
     }
 }
